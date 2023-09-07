f286438b0de74039ce22103a22262a280ecf286b
HumbleOrange <1798292189@qq.com>
2023-09-07 14:50:20 +0800

    UserDao

--- src/main/java/com/example/biubiu/dao/UserDao.java
+++ src/main/java/com/example/biubiu/dao/UserDao.java
@@ -2,16 +2,18 @@ package com.example.biubiu.dao;
 
 import com.example.biubiu.domain.User;
 import com.example.biubiu.util.DButil;
+import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class UserDao {
 
-    public int checkUser(String username,String password) {
+    public User getUserByUsernameAndPassword(String username,String password) {
         Connection cn = null;
         try {
             cn = DButil.getconnection();
@@ -23,19 +25,63 @@ public class UserDao {
         QueryRunner queryRunner = new QueryRunner();
         String sql = "select * from user where username=? and password=?";
         ResultSetHandler<User> h = new BeanHandler<User>(User.class);
-        int n = -1;
         try {
             User p = queryRunner.query(cn,sql, h, username, password);
             if(p!=null) {
-                n = 1;
+                return p;
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return n;
+        return null;
     }
 
-//    public boolean signup(String username,String password){
-//
-//    }
+    public User getUserByUsername(String username) {
+        Connection cn = null;
+        try {
+            cn = DButil.getconnection();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        QueryRunner queryRunner = new QueryRunner();
+        String sql = "select * from user where username=?";
+        ResultSetHandler<User> h = new BeanHandler<User>(User.class);
+        try {
+            User p = queryRunner.query(cn,sql, h, username);
+            if(p!=null) {
+                return p;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public boolean register(String username, String password) {
+        try (Connection conn = DButil.getconnection()) {
+            QueryRunner queryRunner = new QueryRunner();
+
+            // 检查用户名是否已存在
+            String checkQuery = "SELECT COUNT(*) FROM user WHERE username = ?";
+            Long existingUsers = queryRunner.query(conn, checkQuery, new ScalarHandler<Long>(), username);
+
+            if (existingUsers > 0) {
+                // 用户名已存在，无法注册
+                return false;
+            } else {
+                // 插入新用户信息
+                String insertQuery = "INSERT INTO user (username, password, coins, score) VALUES (?, ?, 0, 0)";
+                int rowsInserted = queryRunner.update(conn, insertQuery, username, password);
+
+                return rowsInserted > 0;
+            }
+        } catch (SQLException e) {
+            // 处理数据库异常
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/com/example/biubiu/net/tcp/RequestHandler.java
+++ src/main/java/com/example/biubiu/net/tcp/RequestHandler.java
@@ -1,11 +1,14 @@
 package com.example.biubiu.net.tcp;
 
 
+import com.example.biubiu.dao.UserDao;
+
 import java.io.PrintWriter;
 import java.util.Map;
 
 //用于处理客户端的请求
 public class RequestHandler {
+    private UserDao userDao;
     public PrintWriter output;//客户端
 
     //登录请求
@@ -14,8 +17,10 @@ public class RequestHandler {
         String password = (String)(data.get("password"));
         System.out.println("登录的用户名为" + username);
         System.out.println("登录的密码为" + password);
-        String response = "登录成功";
-        output.println(response);
+        if(userDao.getUserByUsernameAndPassword(username, password) != null)
+            output.println("登录成功");
+        else
+            output.println("登录失败");
     }
 
     //注册请求
@@ -24,8 +29,10 @@ public class RequestHandler {
         String password = (String)(data.get("password"));
         System.out.println("注册的用户名为" + username);
         System.out.println("注册的密码为" + password);
-        String response = "注册成功";
-        output.println(response);
+        if(userDao.register(username, password))
+            output.println("注册成功");
+        else
+            output.println("用户名已存在，请重新注册");
     }
 
     //创建房间
