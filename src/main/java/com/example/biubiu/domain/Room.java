85ca9f7cc28616a2cfd87a0f9157ab602fb8233d
wangxin <294157902@qq.com>
2023-09-08 23:06:50 +0800

    数据类型

--- src/main/java/com/example/biubiu/domain/Room.java
+++ src/main/java/com/example/biubiu/domain/Room.java
@@ -2,8 +2,10 @@ package com.example.biubiu.domain;
 
 import com.example.biubiu.net.tcp.UserClient;
 
+import java.util.ArrayList;
+
 public class Room {
     public int id;
     public int num = 0;    //房间人数
-    public UserClient[] userClients = new UserClient[4];   //玩家列表
+    public ArrayList<UserClient> userClients = new ArrayList<>();   //玩家列表
 }
